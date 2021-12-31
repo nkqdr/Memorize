719f84e9b7e9b554afe47389c30870b93f999df4
Niklas Kuder <nkqdr@users.noreply.github.com>
2021-12-31 11:35:23 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Niklas Kuder on 31.12.21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
